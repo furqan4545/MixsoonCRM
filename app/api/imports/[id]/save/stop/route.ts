20028d0fafde963c786fe50f3af5218052e23403
Furqan Ali <furqan.ali9500@gmail.com>
2026-02-26 19:34:56 +0900

    ddd3

--- app/api/imports/[id]/save/stop/route.ts
+++ app/api/imports/[id]/save/stop/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { SAVE_STOP_REQUESTED } from "@/app/lib/import-save";
 import { requirePermission } from "@/app/lib/rbac";
-import { prisma } from "../../../../../../lib/prisma";
+import { prisma } from "../../../../../lib/prisma";
 
 export async function POST(
   _request: NextRequest,
